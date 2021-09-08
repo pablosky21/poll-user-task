e278adf5b7d2fc69bec9cc6e00122fd8a93e96a8
pablo lincoleo <PabloLincoleo.ElitSoft@latam.com>
2021-09-08 15:48:02 -0400

    se suben cambios para base de datos embebida
    se suben cambios para trabajar con base de datos embebida

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tipo_musica (id, descripcion) VALUES (1, 'jazz');
+INSERT INTO tipo_musica (id, descripcion) VALUES (2, 'Rap');
+INSERT INTO tipo_musica (id, descripcion) VALUES (3, 'Clasica');
--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,2 +1,4 @@
 INSERT INTO tipo_musica (id, descripcion) VALUES (1, 'jazz');
+INSERT INTO tipo_musica (id, descripcion) VALUES (2, 'Rap');
+INSERT INTO tipo_musica (id, descripcion) VALUES (3, 'Clasica');
 INSERT INTO usuario(id, mail,tipo_musicaid) VALUES (1, 'pedro@gmail.com',1);
